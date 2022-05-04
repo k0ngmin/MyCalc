9ffd79dbbefed8e54edd35684c2471239dbdbfd1
k0ngmin <100738240+k0ngmin@users.noreply.github.com>
2022-05-04 09:53:57 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 import setuptools
 
 setuptools.setup(
-  inlude_package_data = True,
+  include_package_data = True,
   name= 'myCalc009',
   version= '0.0.1',
-  descripiton= 'oss-dev Calculator',
+  description= 'oss-dev Calculator',
   author= 'k0ngmin',
   author_email= 'hmongmin808@gmail.com',
   url= 'https://github.com/k0ngmin/myCalc009',
   download_url= 'https://github.com/k0ngmin/myCalc009/archive/refs/tags/v0.zip',
-  install_reqires=['pytest'],
+  install_requires=['pytest'],
   long_description= 'oss-dev caculator python module',
   long_description_content_type= ' text/markdown',
   classifiers=[
